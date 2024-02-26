273b1e20098109bce5e6042350b6bbc84fa6b19a
ogzCode <oguzguc44@gmail.com>
2024-02-26 11:45:11 +0300

    Refactor navigation parsing and add category images

--- backend/app/parser/navigation.py
+++ backend/app/parser/navigation.py
@@ -9,14 +9,18 @@ def parse_navigation(response):
 
         #If there is a category in the link, we only get the category name. 
         #Because we will add it to the end of BASE_URL
-        if a["href"].find("category") == -1:
-            url = ""
-        else:
+        if a["href"].find("category") != -1:
             url = a["href"].split("/category/")[1].replace("/", "")
 
-        nav_list.append({
-            'title': a.text[0] + a.text[1:].lower(),
+            nav_list.append({
+            'title': a.text[0] + turkish_lower(a.text[1:]),
             'url': url
         })
 
     return nav_list
+
+def turkish_lower (text):
+    turkish_letters = {'İ': 'i', 'I': 'ı', 'Ğ': 'ğ', 'Ü': 'ü', 'Ş': 'ş', 'Ö': 'ö', 'Ç': 'ç'}
+    for uppercase, lowercase in turkish_letters.items():
+        text = text.replace(uppercase, lowercase)
+    return text.lower()
--- mobile/src/views/Category.js
+++ mobile/src/views/Category.js
@@ -1,10 +1,118 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Image, ActivityIndicator } from 'react-native';
+import { getNavigation } from '../services/request';
+
+import { slate, sky } from "../assets/style/color";
+import { border } from "../assets/style/border";
+import { size } from "../assets/style/size";
+import { typography } from "../assets/style/typography";
+
+const getImageByCategory = (category) => {
+    switch (category) {
+        case "amerika":
+            return require("../assets/image/abd.jpg");
+        case "avrupa":
+            return require("../assets/image/avrupa.jpg");
+        case "asya":
+            return require("../assets/image/asya.jpg");
+        case "afrika":
+            return require("../assets/image/afrika.jpg");
+        case "okyanusya":
+            return require("../assets/image/okyanusya.jpg");
+        case "balkanlar":
+            return require("../assets/image/balkanlar.jpg");
+        case "digerleri":
+            return require("../assets/image/diger.jpg");
+        case "kafkasya":
+            return require("../assets/image/kafkaslar.jpg");
+        case "ortadogu":
+            return require("../assets/image/ortadogu.jpg");
+        case "turk-dis-politikasi":
+            return require("../assets/image/turkiye.jpg");
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        flex: 1,
+        padding: size[6],
+    },
+    contentContainer: {
+        flex: 1,
+        position: "relative",
+    },
+    thumbBox: {
+        borderRadius: border["rounded"]["lg"],
+    },
+    bottom: {
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.4)",
+        padding: size[2],
+        zIndex: 1,
+    },
+    title: {
+        color: "white",
+        fontSize: typography["fontSizes"]["xl"],
+        fontFamily: "Roboto-Bold",
+        textAlign: "center",
+    },
+    thumbImage: {
+        width: "100%",
+        height: 200,
+        borderRadius: border["rounded"]["lg"],
+    }
+});
+
+const NavItem = ({ item, index }) => {
+    if (item.url === "") return null;
+
+    return (
+        <View style={[styles.contentContainer, {
+            marginRight: index % 2 === 0 ? size[12] : 0,
+            marginBottom: size[6],
+        }]}>
+            <View style={styles.bottom}>
+                <Text style={styles.title}>{item.title}</Text>
+            </View>
+            <View style={styles.thumbBox}>
+                <Image
+                    source={getImageByCategory(item.url)}
+                    style={styles.thumbImage}
+                />
+            </View>
+        </View>
+    )
+}
 
 export default function Category() {
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        getNavigation()
+            .then((res) => {
+                setCategories(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
-        <View>
-            <Text>Category</Text>
+        <View style={styles.container}>
+            {loading && <ActivityIndicator size="large" color={sky["800"]} />}
+            <FlatList
+                data={categories}
+                numColumns={2}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item, index }) => <NavItem item={item} index={index} />
+                }
+            />
         </View>
     )
 }
